1b0fd4c13f64ed4acd10fde618c1350fbc8ea271
Selim Efe <uysalselimefe@gmail.com>
2026-01-16 10:32:21 +0300

    Implement ascending bubble sort for random dataset
    Added source code that: 1. Initializes an array with random values between 0 and 100. 2. Implements nested loops to perform Bubble Sort logic. 3. Displays the final sorted array.

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+int n,i,sayi,array[99],gecici,j;
+
+    srand(time(NULL));
+
+    printf("n i girin ");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++){
+        array[i]=rand()%101;
+    }
+
+    for(i=0;i<n-1;i++){
+        for(j=0;j<n-1-i;j++){
+            if(array[j]>array[j+1]){
+                gecici=array[j];
+                array[j]=array[j+1];
+                array[j+1]=gecici;
+            }
+        }
+    }
+
+    printf("\n");
+
+    for(i=0;i<n;i++){
+        printf("%d ",array[i]);
+    }
+}
